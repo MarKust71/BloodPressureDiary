aba1cc8233092b6d7c316a17d9d3343d1d7669a0
Marek Kustosz <markust71@hotmail.com>
2020-11-28 11:58:13 +0100

    aggregate inputs

--- src/ui/Inputs.tsx
+++ src/ui/Inputs.tsx
@@ -1,43 +1,14 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Text, TextInput } from 'react-native-paper';
+
+import { PressureInput } from 'ui/PressureInput';
 
 export const Inputs = () => {
+    const labels = ['1:', '2:', '3:'];
     return (
         <>
-            <View style={styles.view}>
-                <Text style={styles.text}>1:</Text>
-                <TextInput style={styles.input} mode="outlined" label="SYS" />
-                <TextInput style={styles.input} mode="outlined" label="DIA" />
-                <TextInput style={styles.input} mode="outlined" label="PUL" />
-            </View>
-            <View style={styles.view}>
-                <Text style={styles.text}>2:</Text>
-                <TextInput style={styles.input} mode="outlined" label="SYS" />
-                <TextInput style={styles.input} mode="outlined" label="DIA" />
-                <TextInput style={styles.input} mode="outlined" label="PUL" />
-            </View>
-            <View style={styles.view}>
-                <Text style={styles.text}>3:</Text>
-                <TextInput style={styles.input} mode="outlined" label="SYS" />
-                <TextInput style={styles.input} mode="outlined" label="DIA" />
-                <TextInput style={styles.input} mode="outlined" label="PUL" />
-            </View>
+            {labels.map((item, index) => (
+                <PressureInput label={item} key={index} />
+            ))}
         </>
     );
 };
-
-const styles = StyleSheet.create({
-    text: {
-        width: 20,
-        alignSelf: 'center',
-    },
-    view: {
-        padding: 10,
-        flexDirection: 'row',
-    },
-    input: {
-        width: 110,
-        marginRight: 10,
-    },
-});
---
+++ src/ui/PressureInput.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, TextInput } from 'react-native-paper';
+
+type Props = {
+    label: string;
+};
+
+export const PressureInput = ({ label }: Props) => {
+    return (
+        <View style={styles.view}>
+            <Text style={styles.text}>{label}</Text>
+            <TextInput style={styles.input} mode="outlined" label="SYS" />
+            <TextInput style={styles.input} mode="outlined" label="DIA" />
+            <TextInput style={styles.input} mode="outlined" label="PUL" />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    text: {
+        width: 20,
+        alignSelf: 'center',
+    },
+    view: {
+        padding: 10,
+        flexDirection: 'row',
+    },
+    input: {
+        width: 110,
+        marginRight: 10,
+    },
+});
